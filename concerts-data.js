c322a95b9e49d2a5c2e8622c197a4a85b52cac26
Conor Collins <conorcollins04@gmail.com>
2026-03-18 17:00:25 +0000

    eurovision support

--- concerts-data.js
+++ concerts-data.js
@@ -1,6 +1,7 @@
 const CONCERTS = [
   { artist: "Sigrid",          venue: "Roundhouse",        city: "London", date: "2026-03-13", openers: [] },
-  { festival: "BST Hyde Park", venue: "Hyde Park",          city: "London", date: "2025-07-05", headliners: ["Sabrina Carpenter"], support: ["Clairo", "beabadoobee"] },
+  { festival: "BST Hyde Park",         venue: "Hyde Park", city: "London",    date: "2025-07-05", headliners: ["Sabrina Carpenter"], support: ["Clairo", "beabadoobee"] },
+  { festival: "Eurovision in Concert", venue: "AFAS Live", city: "Amsterdam", date: "2024-04-13", artists: ["Nemo", "Olly Alexander", "Aiko"] },
   { artist: "RAYE",            venue: "The O2",             city: "London", date: "2026-02-26", openers: ["Absolutely", "Amma"] },
   { artist: "Magdalena Bay",   venue: "O2 Academy Brixton", city: "London", date: "2026-02-10", openers: ["Allie X"] },
   { artist: "Allie X",         venue: "KOKO",               city: "London", date: "2024-05-28", openers: ["Tei Shi"] },
