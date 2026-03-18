ed60dd2ee2f894bf621f102cc7661a08d2d4e483
Conor Collins <conorcollins04@gmail.com>
2026-03-18 18:00:56 +0000

    Update concerts-data.js

--- concerts-data.js
+++ concerts-data.js
@@ -1,7 +1,10 @@
 const CONCERTS = [
-  { artist: "Sigrid",          venue: "Roundhouse",        city: "London", date: "2026-03-13", openers: [] },
+  { artist: "Sigrid",          venue: "Roundhouse",           city: "London", date: "2026-03-13", openers: [] },
+  { artist: "Lorde",           venue: "The O2",               city: "London", date: "2025-11-17", openers: ["The Japanese House"] },
+  { artist: "Nemo",            venue: "O2 Academy Islington", city: "London", date: "2025-11-13", openers: ["Punchbag"] },
+  { artist: "Zara Larsson",    venue: "OVO Arena Wembley",    city: "London", date: "2025-11-05", openers: ["Omar Rudberg"] },
   { festival: "BST Hyde Park",         venue: "Hyde Park", city: "London",    date: "2025-07-05", headliners: ["Sabrina Carpenter"], support: ["Clairo", "beabadoobee"] },
-  { festival: "Eurovision in Concert", venue: "AFAS Live", city: "Amsterdam", date: "2024-04-13", artists: ["Nemo", "Olly Alexander", "Aiko"] },
+  { festival: "Eurovision in Concert", venue: "AFAS Live", city: "Amsterdam", date: "2024-04-13", artists: ["Joost", "Besa", "Raiven", "Aiko", "Alexander Rybak", "Slimane", "Natalia Barbu", "Luna", "Saba", "Silvester Belt", "Jerry Heil", "Baby Lasagna", "Isaak", "Nemo", "Olly Alexander", "Windows95man", "Teya Dora", "Mustii", "Hera Björk", "Charlotte Perrelli", "Tali", "Gåte", "Nebulossa", "Alyona Alyona", "Dons", "Bambie Thug", "MEGARA", "Nutsa Buzaladze", "Sarah Bonnici", "Angelina Mango", "Kaleen", "Silia Kapsis", "Marcus & Martinus"] },
   { artist: "RAYE",            venue: "The O2",             city: "London", date: "2026-02-26", openers: ["Absolutely", "Amma"] },
   { artist: "Magdalena Bay",   venue: "O2 Academy Brixton", city: "London", date: "2026-02-10", openers: ["Allie X"] },
   { artist: "Allie X",         venue: "KOKO",               city: "London", date: "2024-05-28", openers: ["Tei Shi"] },
