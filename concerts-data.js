1411ec2d5973c58413638a693fc8bf6dba939a26
Conor Collins <conorcollins04@gmail.com>
2026-03-18 16:19:34 +0000

    Update concerts-data.js

--- concerts-data.js
+++ concerts-data.js
@@ -1,4 +1,7 @@
 const CONCERTS = [
-  { artist: "Sigrid",          venue: "Roundhouse", city: "London", date: "2026-03-13", openers: [] },
-  { artist: "Rina Sawayama",   venue: "Roundhouse", city: "London", date: "2021-11-17", openers: ["HANA", "Ama Jones"] },
+  { artist: "Sigrid",          venue: "Roundhouse",        city: "London", date: "2026-03-13", openers: [] },
+  { artist: "RAYE",            venue: "The O2",             city: "London", date: "2026-02-26", openers: ["Absolutely", "Amma"] },
+  { artist: "Magdalena Bay",   venue: "O2 Academy Brixton", city: "London", date: "2026-02-10", openers: ["Allie X"] },
+  { artist: "Allie X",         venue: "KOKO",               city: "London", date: "2024-05-28", openers: ["Tei Shi"] },
+  { artist: "Rina Sawayama",   venue: "Roundhouse",         city: "London", date: "2021-11-17", openers: ["HANA", "Ama Jones"] },
 ];
