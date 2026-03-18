0f87178e47069dfe2a43cb751cee74fd7e4fb2a4
Conor Collins <conorcollins04@gmail.com>
2026-03-18 16:13:03 +0000

    Concerts

---
+++ concerts-data.js
@@ -0,0 +1,4 @@
+const CONCERTS = [
+  { artist: "Sigrid",          venue: "Roundhouse", city: "London", date: "2026-03-13", openers: [] },
+  { artist: "Rina Sawayama",   venue: "Roundhouse", city: "London", date: "2021-11-17", openers: ["HANA", "Ama Jones"] },
+];
