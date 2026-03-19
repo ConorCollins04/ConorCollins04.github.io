7cc4aad52d670059a7b1938b26ae4e0ac1823446
Conor Collins <conorcollins04@gmail.com>
2026-03-19 17:43:43 +0000

    drag shows

---
+++ drag-shows-data.js
@@ -0,0 +1,22 @@
+const DRAG_SHOWS = [
+  {
+    date: "2024-11-23",
+    venue: "Clapham Grand",
+    city: "London",
+    event: "Nymphia Wind's Banana Ball",
+    headliners: ["Nymphia Wind"],
+    support: ["A'Whora", "Dosa Cat", "Flesh","Sohpia Stardust", "Juno Birch"],
+  },
+  {
+    date: "2025-10-02",
+    venue: "Heaven",
+    city: "London",
+    performers: ["Pasty Kween"],
+  },
+  {
+    date: "2026-03-13",
+    venue: "Admiral Duncan",
+    city: "London",
+    performers: ["Amaya Napa"],
+  },
+];
