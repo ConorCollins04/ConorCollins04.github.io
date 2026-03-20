557b11dce6991a92065f042be3db39a8b57fd6f4
Conor Collins <conorcollins04@gmail.com>
2026-03-20 22:09:41 +0000

    Moving survivor moments into separate data file

---
+++ survivor/moments-data.js
@@ -0,0 +1,28 @@
+const MOMENTS = [
+  {
+    season: "S01", name: "Borneo",
+    moments: [
+      { quote: "Sue Hawk's final tribal council speech", attr: "Sue Hawk", ep: 13, video: "../videos/season-01/snakes-and-rats.mp4" }
+    ]
+  },
+  {
+    season: "S03", name: "Africa",
+    moments: [
+      { quote: "Frank needs prescription drugs", attr: "Kelly Goldsmith", ep: 7, video: "../videos/season-03/frank-loner.mp4" },
+      { quote: "\"It was me... I mean I- I did it.\"", attr: "T-Bird Cooper", ep: 9, video: "../videos/season-03/it-was-me-tbird.mp4" }
+    ]
+  },
+  {
+    season: "S06", name: "The Amazon",
+    moments: [
+      { quote: "\"I didn't know that Jesus had a vested interest in Survivor\"", attr: "Rob Cesternino", ep: 3, video: "../videos/season-06/jesus-vested-interest.mp4" }
+    ]
+  },
+  {
+    season: "S07", name: "Pearl Islands",
+    moments: [
+      { quote: "Rupert steals the other tribe's shoes", attr: "Rupert Boneham", ep: 1, video: "../videos/season-07/rupert-steals-shoes.mp4" },
+      { quote: "\"I can get loud too, what the fuck!\"", attr: "Sandra Diaz-Twine", ep: 3, video: "../videos/season-07/i-can-get-loud-too.mp4" }
+    ]
+  }
+];
