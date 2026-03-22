b01fa340f77b11c1a27da5767cd0e8d3dcb3fcd4
Conor Collins <conorcollins04@gmail.com>
2026-03-22 10:33:03 +0000

    Survivor favourite players page

---
+++ survivor/players-data.js
@@ -0,0 +1,55 @@
+// Season-by-season favourite player picks
+// star:        Your favourite player of the season
+// preMerge:    Your favourite pre-merge player
+// wantBack:    Player you most want to see return
+
+const PLAYER_PICKS = [
+  {
+    seasonId: 1,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 2,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 3,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 4,
+    star:     "Kathy Vavrick O'Brien",
+    preMerge: "Gina Crews",
+    wantBack: "Sean Rector",
+  },
+  {
+    seasonId: 5,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 6,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 7,
+    star:     "",
+    preMerge: "",
+    wantBack: "",
+  },
+  {
+    seasonId: 47,
+    star:     "Genevieve Mushaluk",
+    preMerge: "Anika Dhar",
+    wantBack: "Sam Phalen",
+  },
+];
